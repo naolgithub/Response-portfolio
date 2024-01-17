a1340b5f6e4eb20942ae2af89768ea278bdb10d1
naolgithub <naolcovered@gmail.com>
2024-01-17 17:14:52 +0300

    links is modified

--- lib/changes/links.dart
+++ lib/changes/links.dart
@@ -1,20 +1,19 @@
 // CV
 const String resume =
-    'https://drive.google.com/file/d/1bDNNrkQtNVJbXcdwMSx0crQQvAF_nMwV/view?usp=sharing';
+    'https://drive.google.com/file/d/147SkZDwqK7X4hGuiAVHN975Ho628huuk/view?usp=sharing';
 
 // Github Link
 const String gitHub = 'https://github.com/naolgithub';
 // Github Link
-const String facebook = 'https://facebook.com/sudeshnb';
+const String facebook = 'https://facebook.com/naolaa';
 // Github Link
 const String twitter = 'https://twitter.com/na_ol_aa';
 // Github Link
-const String youtube =
-    'https://www.youtube.com/channel/UCXooUY2oL_eqGhTaZn-ExSg';
+const String youtube = 'https://www.youtube.com/channel/naolaa';
 // Github Link
 const String linkedin = 'https://www.linkedin.com/in/naolabera/';
 // Github Link
-const String buymeacoffee = 'https://www.buymeacoffee.com/sudeshnb';
+const String buymeacoffee = 'https://www.buymeacoffee.com/naolaa';
 // Github Link
 const String instagram = 'https://www.instagram.com/naola.nobody/';
 // socialLinks
@@ -30,9 +29,9 @@ const String instagram = 'https://www.instagram.com/naola.nobody/';
 const String profileImage =
     "https://images.unsplash.com/photo-1503443207922-dff7d543fd0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80";
 //
-// Github Link
+// fiverr Link
 const String fiverr = 'https://www.fiverr.com/sudeshnb';
-// Github Link
+// upwork Link
 const String upwork = 'https://www.upwork.com/freelancers/~015c46144016569e31';
-// Github Link
+// whatsapp Link
 const String whatsapp = 'https://api.whatsapp.com/send?phone=94741325674';
