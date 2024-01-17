6603266a70070245ea28692e3accd334b539d6b4
naolgithub <naolcovered@gmail.com>
2024-01-17 17:15:18 +0300

    strings is modified

--- lib/changes/strings.dart
+++ lib/changes/strings.dart
@@ -23,5 +23,7 @@ String protfolioSubHeading =
 const String aboutMeHeadline =
     'I\'m Naol Abera, a Mobile App developer, Web developer and UI designer.';
 
+// const String aboutMeDetail =
+//     'I\'m a Fresh Graduate as Software Engineer from Quanrt University, UK. I have been developing mobile apps for over 4 years now. I have worked in teams for various startups and helped them in launching their prototypes and got valuable learning experience. Been a CEO/Co-Founder of Onyxsio, Sri Lanka. Currently working as an Associate Software Engineer at Onyxsio.';
 const String aboutMeDetail =
-    'I\'m a Fresh Graduate as Software Engineer from Quanrt University, UK. I have been developing mobile apps for over 4 years now. I have worked in teams for various startups and helped them in launching their prototypes and got valuable learning experience. Been a CEO/Co-Founder of Onyxsio, Sri Lanka. Currently working as an Associate Software Engineer at Onyxsio.';
+    'As a seasoned Mobile developer with 4+ years of experience in Flutter and 3+ years of native Android app dev’t I seek to leverage my expertise in creating exceptional mobile applications with a focus on user experience and performance. Strong technical background in database managements, RESTful APIs, WebSocket, and Flutter Responsive UI, complemented by proficiency in state management, clean architecture, test driven dev’t(TDD), and cloud services(i.e Firebase, GCP, AWS, Supabase and others). With excellent interpersonal skills and a passion for innovation, I am eager to tackle new challenges and contribute to the growth of high-quality, engaging mobile applications.';
