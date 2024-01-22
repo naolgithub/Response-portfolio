86366b975c12b593ce74e023e292418e0dd51a36
naolgithub <naolcovered@gmail.com>
2024-01-22 13:03:13 +0300

    strings modified

--- lib/changes/strings.dart
+++ lib/changes/strings.dart
@@ -4,24 +4,25 @@ String yourname = "I'm Naol Abera,";
 // String hometitle2 = "A Mobile Application\nDeveloper";
 
 String animationtxt1 = " Mobile Application Developer";
-String animationtxt2 = " Content Creater";
-// String animationtxt3 = " Web Developer";
+String animationtxt2 = " Competitive Programmer";
+String animationtxt3 = " Backend Developer";
+String animationtxt4 = " Machine Learning student";
 //
 String contactHeadding = "Let’s try my service now!";
 String contactSubHeadding =
     "Let’s work together and make everything super cute and super useful.";
 //
 String miniDescription =
-    "Freelancer providing services for programming and design content needs. Join me down below and let's get started!";
+    "Mobile developer providing services for different tech companies. Join me down below and let's get started!";
 
 String servicesSubHeading =
-    "Since the beginning of my journey as a freelance designer and developer, I've worked in startups and collaborated with talented people to create digital products for both business and consumer use. I offer a wide range of services, including brand design, programming and teaching.";
+    "Since the beginning of my journey as a mobile developer, I've worked in startups and collaborated with talented people to create digital products for both business and consumer use.";
 //
 
 String protfolioSubHeading =
-    "Since the beginning of my journey as a designer and developer, I have created digital products for business and consumer use. This is a little bit.";
+    "Since the beginning of my journey as a mobile developer, I have created digital products for business and consumer use. This is a little bit.";
 const String aboutMeHeadline =
-    'I\'m Naol Abera, a Mobile App developer, Web developer and UI designer.';
+    'I\'m Naol Abera, a Mobile App developer, backend developer and content creator.';
 
 // const String aboutMeDetail =
 //     'I\'m a Fresh Graduate as Software Engineer from Quanrt University, UK. I have been developing mobile apps for over 4 years now. I have worked in teams for various startups and helped them in launching their prototypes and got valuable learning experience. Been a CEO/Co-Founder of Onyxsio, Sri Lanka. Currently working as an Associate Software Engineer at Onyxsio.';
